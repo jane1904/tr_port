3371dcb68a9c7bf6fb9006880d095bb53c67f320
JANE NGUYEN <janenguyen19490@gmail.com>
2024-11-15 10:58:39 +0700

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+		$(".btn-toggle").click(function(){
+			$("#menu").slideToggle()
+		})
+		
+		$("#dropdown-btn").click(function(){
+			$("#dropdown-menu").slideToggle()
+		})
+	})
+
+
